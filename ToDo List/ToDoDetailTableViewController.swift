d5c6a02256d0536132c4c3f3cda9d7e0d4e5ce54
Samuel Pena <inestbht@gmail.com>
2022-06-05 23:19:11 -0500

    SAVED STATE - Ch. 5.3 - Section Completed

---
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDo List
+//
+//  Created by Samuel Pena on 6/5/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+}
