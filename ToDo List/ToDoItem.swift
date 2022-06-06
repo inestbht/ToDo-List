fb9ab8b2503024a53c1bec284de8d2a173231d17
Samuel Pena <inestbht@gmail.com>
2022-06-06 13:54:33 -0500

    SAVED STATE = Ch. 5.6 - Structs

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Samuel Pena on 6/6/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
