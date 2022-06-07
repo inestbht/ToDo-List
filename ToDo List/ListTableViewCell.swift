5d82eb24bc102aec2435ebbabc9056f7f46e6eba
Samuel Pena <inestbht@gmail.com>
2022-06-07 00:31:57 -0500

    SAVED STATE - Ch. 5.11 - Adding Checkmarks via Custom Table View Cells + Protocols

---
+++ ToDo List/ListTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ListTableViewCell.swift
+//  ToDo List
+//
+//  Created by Samuel Pena on 6/7/22.
+//  Copyright © 2022 Samuel Pena. All rights reserved.
+//
+
+import UIKit
+
+protocol ListTableViewCellDelegate: class {
+    func checkBoxToggle(sender: ListTableViewCell)
+}
+
+class ListTableViewCell: UITableViewCell {
+    
+    weak var delegate: ListTableViewCellDelegate?
+
+    @IBOutlet weak var checkBoxButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBAction func checkToggled(_ sender: UIButton) {
+        delegate?.checkBoxToggle(sender: self)
+    }
+    
+}
