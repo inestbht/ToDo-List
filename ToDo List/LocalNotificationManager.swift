3247ac7803623febf8d7951c388b38fe3c5cb378
Samuel Pena <inestbht@gmail.com>
2022-06-07 21:30:22 -0500

    SAVED STATE - Ch. 5.16 - Extensions for Alerts, Threads, and Improving the User Experience

--- ToDo List/LocalNotificationManager.swift
+++ ToDo List/LocalNotificationManager.swift
@@ -6,12 +6,12 @@
 //  Copyright © 2022 Samuel Pena. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import UserNotifications
 
 struct LocalNotificationManager {
     
-    static func autherizeLocalNotifications() {
+    static func autherizeLocalNotifications(viewController: UIViewController) {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
             guard error == nil else {
                 print("😡 ERROR: \(error!.localizedDescription)")
@@ -21,7 +21,26 @@ struct LocalNotificationManager {
                 print("✅ Notifications Authorization Granted!")
             } else {
                 print("🚫 The user has declined notifications!")
-                //TODO: Put an alert in here telling the user what to do
+                DispatchQueue.main.async {
+                    viewController.oneButtonAlert(title: "User Has Not Allowed Notifications", message: "To receive alerts for reminders, open the Settings app, select To Do List > Notifications > Allow Notifications.")
+                }
+            }
+        }
+    }
+    
+    static func isAuthorized(completed: @escaping (Bool)->() ) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            guard error == nil else {
+                print("😡 ERROR: \(error!.localizedDescription)")
+                completed(false)
+                return
+            }
+            if granted {
+                print("✅ Notifications Authorization Granted!")
+                completed(true)
+            } else {
+                print("🚫 The user has declined notifications!")
+                completed(false)
             }
         }
     }
